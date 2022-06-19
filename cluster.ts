abd6712f3be5de9ae9c5fa6665d27a7ce600360d
Maksim Pevnev <drummen11@gmail.com>
2022-06-20 02:52:21 +0400

    develop: add cluster

---
+++ cluster.ts
@@ -0,0 +1,21 @@
+const cluster = require('cluster');
+import { serverStart } from './src/server';
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        if (signal) {
+            console.log(`worker was killed by signal: ${signal}`);
+        } else if (code !== 0) {
+            console.log(`worker exited with error code: ${code}`);
+        } else {
+            console.log('worker success!');
+        }
+    });
+} else {
+    serverStart();
+}
